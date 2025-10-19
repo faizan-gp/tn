f52442c62e7c6d4a9c9b22807cd661e5624273b1
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2025-10-20 01:02:42 +0800

    config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**', // This allows any path from placehold.co
+      },
+    ],
+  },
 };
 
 export default nextConfig;
